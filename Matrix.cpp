0600d3663b876677112bac41d390fb3456233aae
aoaomidri <aoaomidori2723@gmail.com>
2023-06-06 23:57:56 +0900

    隙間開けただけ

--- Matrix.cpp
+++ Matrix.cpp
@@ -67,18 +67,21 @@ Matrix4x4 Matrix::Inverce(const Matrix4x4& mat) {
 		mat.m[0][0] * mat.m[1][3] * mat.m[2][2] * mat.m[3][1] -
 		mat.m[0][0] * mat.m[1][2] * mat.m[2][1] * mat.m[3][3] -
 		mat.m[0][0] * mat.m[1][1] * mat.m[2][3] * mat.m[3][2] -
+
 		mat.m[0][1] * mat.m[1][0] * mat.m[2][2] * mat.m[3][3] -
 		mat.m[0][2] * mat.m[1][0] * mat.m[2][3] * mat.m[3][1] -
 		mat.m[0][3] * mat.m[1][0] * mat.m[2][1] * mat.m[3][2] +
 		mat.m[0][3] * mat.m[1][0] * mat.m[2][2] * mat.m[3][1] +
 		mat.m[0][2] * mat.m[1][0] * mat.m[2][1] * mat.m[3][3] +
 		mat.m[0][1] * mat.m[1][0] * mat.m[2][3] * mat.m[3][2] +
+
 		mat.m[0][1] * mat.m[1][2] * mat.m[2][0] * mat.m[3][3] +
 		mat.m[0][2] * mat.m[1][3] * mat.m[2][0] * mat.m[3][1] +
 		mat.m[0][3] * mat.m[1][1] * mat.m[2][0] * mat.m[3][2] -
 		mat.m[0][3] * mat.m[1][2] * mat.m[2][0] * mat.m[3][1] -
 		mat.m[0][2] * mat.m[1][1] * mat.m[2][0] * mat.m[3][3] -
 		mat.m[0][1] * mat.m[1][3] * mat.m[2][0] * mat.m[3][2] -
+		
 		mat.m[0][1] * mat.m[1][2] * mat.m[2][3] * mat.m[3][0] -
 		mat.m[0][2] * mat.m[1][3] * mat.m[2][1] * mat.m[3][0] -
 		mat.m[0][3] * mat.m[1][1] * mat.m[2][2] * mat.m[3][0] +
